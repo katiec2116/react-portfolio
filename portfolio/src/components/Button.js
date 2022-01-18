b22a6cadfb5fe466b4f4e1814ba7c69dc9f3b216
katiec <katiecampbell21@outlook.com>
2022-01-18 16:09:31 -0500

    add button and styling

---
+++ portfolio/src/components/Button.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { Link } from "react-scroll";
+
+export const Button = styled(Link)`
+  background: transparent;
+  padding: ${({ large }) => (large ? "14px 48px" : "12px 30px;")};
+  color: #ffffff;
+  font-family: poppins;
+  text-transform: uppercase;
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+  border: 4px solid #75c9c0;
+  margin-left: 15px;
+  margin-right: 15px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #75c9c0;
+  }
+`;
